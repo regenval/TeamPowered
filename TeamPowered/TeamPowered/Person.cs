dd254fd0012807aef144f5a80b342d2c6ea5aede
vivia_000 <vivia_000@VITASOY>
2016-08-15 06:07:50 -0700

    Split Name into first and last name

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
